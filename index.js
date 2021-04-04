6599a4431d68ba36ae42967f1a2f539ed00a1744
saymon-shoab <mdsaymonshoab@gmail.com>
2021-04-04 07:32:11 +0600

    my server code

---
+++ index.js
@@ -0,0 +1,76 @@
+
+const express = require('express');
+const bodyParser = require('body-Parser');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+const port = 5000;
+const uri = `mongodb+srv://${process.env.DV_USER}:${process.env.DV_PASS}@cluster0.5h4lz.mongodb.net/${process.env.DV_NAME}?retryWrites=true&w=majority`;
+
+
+
+
+const app = express()
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const productsCollection = client.db("emajohnstore").collection("products");
+  const ordersCollection = client.db("emajohnstore").collection("orders");
+     app.post('/addProduct', (req, res)=>{
+         const products= req.body;
+        // console.log(product)
+        productsCollection.insertOne(products)
+         .then(result=>{
+             console.log(result.insertedCount)
+             res.send(result.insertedCount)
+         })
+     })
+     console.log('database connected')
+
+     app.get('/products', (req, res)=>{
+        productsCollection.find({})
+        .toArray((err,documents)=>{
+            res.send(documents)
+        })
+     })
+
+
+
+     app.get('/product/:key', (req, res)=>{
+        productsCollection.find({key: req.params.key})
+        .toArray((err,documents)=>{
+            res.send(documents[0])
+        })
+     })
+
+     app.post('/productsByKeys', (req, res)=>{
+         const productKeys= req.body;
+        productsCollection.find({key: {$in:productKeys}})
+        .toArray((err,documents)=>{
+            res.send(documents);
+        })
+     })
+    
+
+     app.post('/addOrder', (req, res)=>{
+        const order= req.body;
+        ordersCollection.insertOne(order)
+        .then(result=>{
+            console.log(result.insertedCount)
+            res.send(result.insertedCount > 0)
+        })
+    })
+
+
+
+});
+
+
+
+
+
+app.listen(process.env.PORT || port)
